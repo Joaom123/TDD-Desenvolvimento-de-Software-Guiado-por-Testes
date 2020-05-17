1d294ba08a1cbe6c7de178c17bf8334e37652fec
João Marcus <joaomarcus12769@gmail.com>
2020-05-16 22:58:57 -0300

    aceita siglas

--- src/camelCaseConversor/CamelCaseConversor.java
+++ src/camelCaseConversor/CamelCaseConversor.java
@@ -1,20 +1,21 @@
 package camelCaseConversor;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+
 /** Requisitos
  * nome - “nome” - Ok
  * Nome - “nome” - Ok
  * nomeComposto - “nome”, “composto” - Ok
  * NomeComposto - “nome”, “composto” - Ok
- * CPF - “CPF”
- * numeroCPF - “numero”, “CPF”
- * numeroCPFContribuinte - “numero”, “CPF”, “contribuinte”
+ * CPF - “CPF” - Ok
+ * numeroCPF - “numero”, “CPF” - OK
+ * numeroCPFContribuinte - “numero”, “CPF”, “contribuinte” - Ok
  * recupera10Primeiros - “recupera”, “10”, “primeiros”
  * 10Primeiros - Inválido → não deve começar com números
  * nome#Composto - Inválido → caracteres especiais não são permitidos, somente letras e números
  */
+
 public class CamelCaseConversor {
 	public static List<String> palavras;
 	
@@ -26,8 +27,12 @@ public class CamelCaseConversor {
 		for (int i = 0; i < original.length(); i++) {
 		    char letraAtual = original.charAt(i);
 		    
+		    char letraSeguinte = 'a';
+		    if(i == original.length())
+		    	original.charAt(i+1);
+		    
 		    if(Character.isUpperCase(letraAtual)) {
-		    	adicionaPalavraNaoVazia(palavraSeparada);
+	    		adicionaPalavraNaoVazia(palavraSeparada);
 		    	palavraSeparada = "";
 		    }
 		    
@@ -37,7 +42,7 @@ public class CamelCaseConversor {
 		
 		adicionaPalavraNaoVazia(palavraSeparada);
 		
-		return palavras;
+		return juntaSigla(palavras);
 	}
 	
 	public static void adicionaPalavraNaoVazia(String palavra) {
@@ -47,6 +52,30 @@ public class CamelCaseConversor {
 	
 	public static void adicionaPalavra(String palavra) {
 		palavras.add(palavra);
+	}
+	
+	public static List<String> juntaSigla(List<String> palavras) {
+		List<String> palavrasAux = new ArrayList<String>();
+		String aux = "";
+		
+		for (String palavra : palavras) {
+			if (palavra.length() == 1) {
+				aux += palavra;
+			}else {
+				if(!aux.isEmpty()) {
+					palavrasAux.add(aux.toUpperCase());
+					aux = "";
+				}
+				palavrasAux.add(palavra);
+			}
+		}
+		
+		if(!aux.isEmpty()) {
+			palavrasAux.add(aux.toUpperCase());
+			aux = "";
+		}
+		
+		return palavrasAux;
 	}
 
 }
